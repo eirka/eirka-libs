07adeef576d4e5f69fa37869153cbf7587672037
techjanitor <puntme@gmail.com>
2015-11-11 22:39:22 -0600

    enhance print config

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	"fmt"
+	"strings"
 )
 
 var Settings *Config
@@ -79,16 +80,16 @@ type Config struct {
 }
 
 func Print() {
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("%-20v\n\n", "General")
 	fmt.Printf("%-20v%40v\n", "Guest Posting", Settings.General.GuestPosting)
 	fmt.Printf("%-20v%40v\n", "Auto Registration", Settings.General.AutoRegistration)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Antispam")
 	fmt.Printf("%-20v%40v\n", "Key", Settings.Antispam.AntispamKey)
 	fmt.Printf("%-20v%40v\n", "Cookie Name", Settings.Antispam.CookieName)
 	fmt.Printf("%-20v%40v\n", "Cookie Value", Settings.Antispam.CookieValue)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Limits")
 	fmt.Printf("%-20v\n\n", "Images")
 	fmt.Printf("%-20v%40v\n", "Min Width", Settings.Limits.ImageMinWidth)
@@ -110,20 +111,20 @@ func Print() {
 	fmt.Printf("%-20v%40v\n", "Tag Max", Settings.Limits.TagMaxLength)
 	fmt.Printf("%-20v%40v\n", "Tag Min", Settings.Limits.TagMinLength)
 	fmt.Printf("%-20v%40v\n", "Max Param Size", Settings.Limits.ParamMaxSize)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Akismet")
 	fmt.Printf("%-20v%40v\n", "Key", Settings.Akismet.Key)
 	fmt.Printf("%-20v%40v\n", "Host", Settings.Akismet.Host)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Stop Forum Spam")
 	fmt.Printf("%-20v%40v\n", "Confidence", Settings.StopForumSpam.Confidence)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Amazon")
 	fmt.Printf("%-20v%40v\n", "Region", Settings.Amazon.Region)
 	fmt.Printf("%-20v%40v\n", "Bucket", Settings.Amazon.Bucket)
 	fmt.Printf("%-20v%40v\n", "Id", Settings.Amazon.Id)
 	fmt.Printf("%-20v%40v\n", "Key", Settings.Amazon.Key)
-
+	fmt.Println(strings.Repeat("*", 60))
 	fmt.Printf("\n%-20v\n\n", "Google")
 	fmt.Printf("%-20v%40v\n", "Auth", Settings.Google.Auth)
 	fmt.Printf("%-20v%40v\n", "Bucket", Settings.Google.Bucket)
