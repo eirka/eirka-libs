514b532ecff1aafa6ebc23977975ed38e9cf68d2
techjanitor <puntme@gmail.com>
2015-11-11 20:44:58 -0600

    add middleware and utils

---
+++ validate/validate.go
@@ -0,0 +1,38 @@
+package validate
+
+import (
+	"github.com/gin-gonic/gin"
+
+	e "github.com/techjanitor/pram-libs/errors"
+)
+
+// ValidateParams will loop through the route parameters to make sure theyre uint
+func ValidateParams() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		if c.Params != nil {
+
+			var params []uint
+
+			for _, param := range c.Params {
+
+				pid, err := ValidateParam(param.Value)
+				if err != nil {
+					c.JSON(e.ErrorMessage(e.ErrInvalidParam))
+					c.Error(e.ErrInvalidParam)
+					c.Abort()
+					return
+				}
+
+				params = append(params, pid)
+
+			}
+
+			c.Set("params", params)
+
+		}
+
+		c.Next()
+
+	}
+}
