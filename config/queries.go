3adede11bde5a10cd4ecc416f91e75ffd687e101
techjanitor <puntme@gmail.com>
2015-11-11 21:46:45 -0600

    add local config

--- config/queries.go
+++ config/queries.go
@@ -17,167 +17,167 @@ func GetDatabaseSettings() {
 	}
 	defer ps.Close()
 
-	err = ps.QueryRow("antispam_key").Scan(&config.Settings.Antispam.AntispamKey)
+	err = ps.QueryRow("antispam_key").Scan(&Settings.Antispam.AntispamKey)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("antispam_cookiename").Scan(&config.Settings.Antispam.CookieName)
+	err = ps.QueryRow("antispam_cookiename").Scan(&Settings.Antispam.CookieName)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("antispam_cookievalue").Scan(&config.Settings.Antispam.CookieValue)
+	err = ps.QueryRow("antispam_cookievalue").Scan(&Settings.Antispam.CookieValue)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("image_minwidth").Scan(&config.Settings.Limits.ImageMinWidth)
+	err = ps.QueryRow("image_minwidth").Scan(&Settings.Limits.ImageMinWidth)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("image_minheight").Scan(&config.Settings.Limits.ImageMinHeight)
+	err = ps.QueryRow("image_minheight").Scan(&Settings.Limits.ImageMinHeight)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("image_maxwidth").Scan(&config.Settings.Limits.ImageMaxWidth)
+	err = ps.QueryRow("image_maxwidth").Scan(&Settings.Limits.ImageMaxWidth)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("image_maxheight").Scan(&config.Settings.Limits.ImageMaxHeight)
+	err = ps.QueryRow("image_maxheight").Scan(&Settings.Limits.ImageMaxHeight)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("image_maxsize").Scan(&config.Settings.Limits.ImageMaxSize)
+	err = ps.QueryRow("image_maxsize").Scan(&Settings.Limits.ImageMaxSize)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("webm_maxlength").Scan(&config.Settings.Limits.WebmMaxLength)
+	err = ps.QueryRow("webm_maxlength").Scan(&Settings.Limits.WebmMaxLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("thread_postsmax").Scan(&config.Settings.Limits.PostsMax)
+	err = ps.QueryRow("thread_postsmax").Scan(&Settings.Limits.PostsMax)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("comment_maxlength").Scan(&config.Settings.Limits.CommentMaxLength)
+	err = ps.QueryRow("comment_maxlength").Scan(&Settings.Limits.CommentMaxLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("comment_minlength").Scan(&config.Settings.Limits.CommentMinLength)
+	err = ps.QueryRow("comment_minlength").Scan(&Settings.Limits.CommentMinLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("title_maxlength").Scan(&config.Settings.Limits.TitleMaxLength)
+	err = ps.QueryRow("title_maxlength").Scan(&Settings.Limits.TitleMaxLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("title_minlength").Scan(&config.Settings.Limits.TitleMinLength)
+	err = ps.QueryRow("title_minlength").Scan(&Settings.Limits.TitleMinLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("name_maxlength").Scan(&config.Settings.Limits.NameMaxLength)
+	err = ps.QueryRow("name_maxlength").Scan(&Settings.Limits.NameMaxLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("name_minlength").Scan(&config.Settings.Limits.NameMinLength)
+	err = ps.QueryRow("name_minlength").Scan(&Settings.Limits.NameMinLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("tag_maxlength").Scan(&config.Settings.Limits.TagMaxLength)
+	err = ps.QueryRow("tag_maxlength").Scan(&Settings.Limits.TagMaxLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("tag_minlength").Scan(&config.Settings.Limits.TagMinLength)
+	err = ps.QueryRow("tag_minlength").Scan(&Settings.Limits.TagMinLength)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("thumbnail_maxwidth").Scan(&config.Settings.Limits.ThumbnailMaxWidth)
+	err = ps.QueryRow("thumbnail_maxwidth").Scan(&Settings.Limits.ThumbnailMaxWidth)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("thumbnail_maxheight").Scan(&config.Settings.Limits.ThumbnailMaxHeight)
+	err = ps.QueryRow("thumbnail_maxheight").Scan(&Settings.Limits.ThumbnailMaxHeight)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("param_maxsize").Scan(&config.Settings.Limits.ParamMaxSize)
+	err = ps.QueryRow("param_maxsize").Scan(&Settings.Limits.ParamMaxSize)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("guest_posting").Scan(&config.Settings.General.GuestPosting)
+	err = ps.QueryRow("guest_posting").Scan(&Settings.General.GuestPosting)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("auto_registration").Scan(&config.Settings.General.AutoRegistration)
+	err = ps.QueryRow("auto_registration").Scan(&Settings.General.AutoRegistration)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("akismet_key").Scan(&config.Settings.Akismet.Key)
+	err = ps.QueryRow("akismet_key").Scan(&Settings.Akismet.Key)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("akismet_host").Scan(&config.Settings.Akismet.Host)
+	err = ps.QueryRow("akismet_host").Scan(&Settings.Akismet.Host)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("sfs_confidence").Scan(&config.Settings.StopForumSpam.Confidence)
+	err = ps.QueryRow("sfs_confidence").Scan(&Settings.StopForumSpam.Confidence)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("amazon_region").Scan(&config.Settings.Amazon.Region)
+	err = ps.QueryRow("amazon_region").Scan(&Settings.Amazon.Region)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("amazon_bucket").Scan(&config.Settings.Amazon.Bucket)
+	err = ps.QueryRow("amazon_bucket").Scan(&Settings.Amazon.Bucket)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("amazon_id").Scan(&config.Settings.Amazon.Id)
+	err = ps.QueryRow("amazon_id").Scan(&Settings.Amazon.Id)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("amazon_key").Scan(&config.Settings.Amazon.Key)
+	err = ps.QueryRow("amazon_key").Scan(&Settings.Amazon.Key)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("google_auth").Scan(&config.Settings.Google.Auth)
+	err = ps.QueryRow("google_auth").Scan(&Settings.Google.Auth)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("google_bucket").Scan(&config.Settings.Google.Bucket)
+	err = ps.QueryRow("google_bucket").Scan(&Settings.Google.Bucket)
 	if err != nil {
 		panic(err)
 	}
 
-	err = ps.QueryRow("google_key").Scan(&config.Settings.Google.Key)
+	err = ps.QueryRow("google_key").Scan(&Settings.Google.Key)
 	if err != nil {
 		panic(err)
 	}
