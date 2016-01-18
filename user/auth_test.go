bd80c213d53bfbc2b263c520a2a4fb87a28dc17b
techjanitor <puntme@gmail.com>
2016-01-17 19:26:19 -0600

    add form test to auth middleware

--- user/auth_test.go
+++ user/auth_test.go
@@ -1,9 +1,11 @@
 package user
 
 import (
+	"bytes"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/stretchr/testify/assert"
+	"mime/multipart"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -16,9 +18,23 @@ func performRequest(r http.Handler, method, path string) *httptest.ResponseRecor
 	return w
 }
 
-func performJwtRequest(r http.Handler, method, path, token string) *httptest.ResponseRecorder {
+func performJwtHeaderRequest(r http.Handler, method, path, token string) *httptest.ResponseRecorder {
 	req, _ := http.NewRequest(method, path, nil)
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %s", token))
+	w := httptest.NewRecorder()
+	r.ServeHTTP(w, req)
+	return w
+}
+
+func performJwtFormRequest(r http.Handler, method, path, token string) *httptest.ResponseRecorder {
+	var b bytes.Buffer
+
+	mw := multipart.NewWriter(&b)
+	mw.WriteField("access_token", token)
+	mw.Close()
+
+	req, _ := http.NewRequest(method, path, &b)
+	req.Header.Set("Content-Type", mw.FormDataContentType())
 	w := httptest.NewRecorder()
 	r.ServeHTTP(w, req)
 	return w
@@ -52,7 +68,7 @@ func TestAuthSecret(t *testing.T) {
 
 }
 
-func TestAuthToken(t *testing.T) {
+func TestAuthHeaderToken(t *testing.T) {
 
 	Secret = "secret"
 
@@ -83,7 +99,7 @@ func TestAuthToken(t *testing.T) {
 
 	Secret = "changed"
 
-	second := performJwtRequest(router, "GET", "/", badtoken)
+	second := performJwtHeaderRequest(router, "GET", "/", badtoken)
 
 	assert.Equal(t, second.Code, 401, "HTTP request code should match")
 
@@ -92,8 +108,49 @@ func TestAuthToken(t *testing.T) {
 		assert.NotEmpty(t, goodtoken, "token should be returned")
 	}
 
-	third := performJwtRequest(router, "GET", "/", goodtoken)
+	third := performJwtHeaderRequest(router, "GET", "/", goodtoken)
 
 	assert.Equal(t, third.Code, 200, "HTTP request code should match")
 
+}
+
+func TestAuthFormToken(t *testing.T) {
+
+	Secret = "secret"
+
+	gin.SetMode(gin.ReleaseMode)
+
+	router := gin.New()
+
+	// route is protected
+	router.Use(Auth(true))
+
+	router.GET("/", func(c *gin.Context) {
+		c.String(200, "OK")
+		return
+	})
+
+	user := DefaultUser()
+	user.SetId(2)
+	user.SetAuthenticated()
+
+	badtoken, err := user.CreateToken()
+	if assert.NoError(t, err, "An error was not expected") {
+		assert.NotEmpty(t, badtoken, "token should be returned")
+	}
+
+	Secret = "changed"
+
+	first := performJwtFormRequest(router, "GET", "/", badtoken)
+
+	assert.Equal(t, first.Code, 401, "HTTP request code should match")
+
+	goodtoken, err := user.CreateToken()
+	if assert.NoError(t, err, "An error was not expected") {
+		assert.NotEmpty(t, goodtoken, "token should be returned")
+	}
+
+	second := performJwtFormRequest(router, "GET", "/", goodtoken)
+
+	assert.Equal(t, second.Code, 200, "HTTP request code should match")
 }
