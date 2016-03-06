3272bbf97c9d9ef0c9e746511cd0a712233f5b9b
techjanitor <puntme@gmail.com>
2016-03-05 18:33:02 -0600

    add user password sql check

--- user/user_test.go
+++ user/user_test.go
@@ -355,3 +355,29 @@ func TestFromNameBadId(t *testing.T) {
 	}
 
 }
+
+func TestCheckDuplicateGood(t *testing.T) {
+
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	rows := sqlmock.NewRows([]string{"count"}).AddRow(0)
+
+	mock.ExpectQuery("select count(*) from users where user_name").WillReturnRows(rows)
+
+	assert.False(t, CheckDuplicate(), "Should not be a duplicate")
+
+}
+
+func TestCheckDuplicateBad(t *testing.T) {
+
+	mock, err := db.NewTestDb()
+	assert.NoError(t, err, "An error was not expected")
+
+	rows := sqlmock.NewRows([]string{"count"}).AddRow(1)
+
+	mock.ExpectQuery("select count(*) from users where user_name").WillReturnRows(rows)
+
+	assert.True(t, CheckDuplicate(), "Should be a duplicate")
+
+}
